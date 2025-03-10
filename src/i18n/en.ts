a56043d2e7e9777df013df088b57c50162e4dbc3
MoJie <mojie@z-srm.com>
2025-03-10 15:37:22 +0800

    feat(all): 实现鼠标拖动旋转
    1.工具栏功能
        a.向上视角
        b.向下视角
        c.向左视角
        d.向右视角
        e.放大
        f.缩小
    2.自动旋转
    3.加载效果

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -6,5 +6,9 @@ export const en = {
     "show-list": "File visibility",
     "switch": "Toolbar",
     "auto-view": "Automatic Preview",
+    "up": "Rotate the perspective upwards",
+    "down": "Rotate the perspective downwards",
+    "left": "Rotate the perspective leftwards",
+    "right": "Rotate the perspective rightwards",
     "noFindFile": "No FindFile(The fileList property is a required field).",
 }
